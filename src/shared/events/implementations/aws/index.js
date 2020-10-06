b63651cd26a8e880af40a9f71a287509e46e7318
Mehmet Ali Peker <maps6134@hotmail.com>
2020-10-06 03:52:30 +0300

    SQS client added (#12)
    * SQS client added
    * Added errorHandlerWrapper

--- src/shared/events/implementations/aws/index.js
+++ src/shared/events/implementations/aws/index.js
@@ -1,9 +1,56 @@
-function enqueue(topic, payload) {
-    console.log(`aws enqueue: ${topic} ${JSON.stringify(payload, null, 4)}`);
+const AWS = require('aws-sdk');
+
+const sqs = new AWS.SQS();
+const { AWS_REGION, AWS_ACCOUNT_ID } = process.env;
+
+
+function errorHandlerWrapper(action) {
+    try {
+        return action();
+    }
+    catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
 
-function dequeue(topic) {
-    console.log(`aws dequeue: ${topic}`);
+async function enqueueInternal(topic, payload) {
+    const queueUrl = `https://sqs.${AWS_REGION}.amazonaws.com/${AWS_ACCOUNT_ID}/${topic}`;
+
+    const params = {
+        MessageBody: JSON.stringify(payload),
+        QueueUrl: queueUrl,
+    };
+
+    const result = await sqs.sendMessage(params);
+
+    return result;
+}
+
+function enqueue(...args) {
+    return errorHandlerWrapper(() => enqueueInternal(...args));
+}
+
+async function dequeueInternal(topic, maxNumberOfMessages = 10) {
+    const queueUrl = `https://sqs.${AWS_REGION}.amazonaws.com/${AWS_ACCOUNT_ID}/${topic}`;
+
+    const params = {
+        QueueUrl: queueUrl,
+        MaxNumberOfMessages: maxNumberOfMessages,
+    };
+
+    const result = await sqs.receiveMessage(params);
+
+    result.Messages = result.Messages.map(message => ({
+        ...message,
+        Body: JSON.parse(message.Body),
+    }));
+
+    return result;
+}
+
+function dequeue(...args) {
+    return errorHandlerWrapper(() => dequeueInternal(...args));
 }
 
 module.exports = {
